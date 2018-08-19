7411f642fcd28b3b171ea3e4da0b65e74fb647f7
zhanghui <979292329@qq.com>
2018-08-19 18:31:56 +0800

    1

---
+++ taotao-common/src/main/java/com/taotao/common/pojo/EasyUITreeNode.java
@@ -0,0 +1,31 @@
+package com.taotao.common.pojo;
+
+public class EasyUITreeNode {
+    private long id;
+    private String text;
+    private String state;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
